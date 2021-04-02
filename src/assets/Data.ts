4dde18aaef7e0daa6e973c18b253ce551d29425a
김지운 <expect.ta@gmail.com>
2021-04-02 14:09:04 +0900

    [CLIENT] ADD : 디렉터리 폴더 구분을 위한 추가
    관심사 분리를 위한 폴더 분리

---
+++ src/assets/Data.ts
@@ -0,0 +1,92 @@
+export const arr = [
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+  {
+    content: "안녕하세요",
+  },
+];
---
+++ src/assets/PrintSpeach.ts
@@ -0,0 +1,1 @@
+export function PrintSpeach(script: any) {}
