c73828eb08f73d45623fa86b2524b096c5e6659c
김지운 <expect.ta@gmail.com>
2021-04-02 14:03:33 +0900

    [CLINET] FIX : 최상위 컴포넌트 route 분기처리
    url별 화면 분기처리를 위한 routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
 import React from "react";
+import { Router, Switch, Route } from "react-router-dom";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import theme from "./assets/theme";
+import { createMemoryHistory } from "history";
+import { Main, Login, Inventory, UserInfo, Chat } from "./pages/Index";
 
 export default function App() {
-  return <>1</>;
+  const history = createMemoryHistory();
+  return (
+    <Router history={history}>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route exact path="/" render={() => <Main></Main>}></Route>
+          <Route exact path="/login" render={() => <Login></Login>}></Route>
+          <Route
+            exact
+            path="/inventory"
+            render={() => <Inventory></Inventory>}
+          ></Route>
+          <Route
+            exact
+            path="/userInfo"
+            render={() => <UserInfo></UserInfo>}
+          ></Route>
+          <Route exact path="/chat" render={() => <Chat></Chat>}></Route>
+        </Switch>
+      </ThemeProvider>
+    </Router>
+  );
 }
+
+const GlobalStyle = createGlobalStyle`
+* {
+	box-sizing: border-box;
+	scroll-behavior: smooth;
+}
+body{                        
+	width:700px;
+	height:100vh;
+	margin : 0 auto;
+	padding: 0;
+}
+`;
