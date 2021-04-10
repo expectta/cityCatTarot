667389b35d6d6a4f666151919bbad0dd11b4dfa5
김지운 <expect.ta@gmail.com>
2021-04-10 23:46:18 +0900

    [CLIENT] FIX : main 화면 menu 이벤트 작성 및 css 디자인 변경
    main 화면 menu 이벤트 작성 및 css 디자인 변경

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,66 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
-import { Link, useRouteMatch } from "react-router-dom";
+import {
+  Route,
+  Switch,
+  Link,
+  useRouteMatch,
+  useHistory,
+} from "react-router-dom";
 import { Slide } from "../components/Index";
-export default function Main() {
+import { BsJustify } from "react-icons/bs";
+import Inventory from "./Inventory";
+interface props {
+  loginInfo: any;
+  openModal: any;
+  handleLogOut: any;
+}
+export default function Main({ loginInfo, openModal, handleLogOut }: props) {
+  const history = useHistory();
   const urlMatch = useRouteMatch();
+  const [menu, setMenu] = useState({
+    name: "",
+  });
+
+  const handleChangeMenu = (menuName: string) => {
+    setMenu({
+      name: menuName,
+    });
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    handleLogOut();
+    history.replace("/");
+  };
   return (
     <>
+      {/* <Slide></Slide> */}
       <Container>
         <Wrapper>
-          <Nav>
-            <Login to="/login">login</Login>
-          </Nav>
-          <SliderWrapper>{/* <Slide></Slide> */}</SliderWrapper>
+          <SliderWrapper>
+            <Video src="./mainBener.mp4" muted autoPlay loop></Video>
+          </SliderWrapper>
           <SubTarotCard>
-            <TodayTarot to="/chat"></TodayTarot>
-            <SomeTarot to="/chat"></SomeTarot>
+            <TodayTarot to="/chat">
+              <TodayImg src="./today.jpg"></TodayImg>
+            </TodayTarot>
+            <SomeTarot to="/chat">
+              <SomeImg src="./some.jpg"></SomeImg>
+            </SomeTarot>
           </SubTarotCard>
         </Wrapper>
         <SideMenu>
-          <SlideButton>{"보관함"}</SlideButton>
-          <SlideContents></SlideContents>
+          <SlideButton>
+            <Icon size={36}></Icon>
+          </SlideButton>
+          <SlideContents>
+            <Nav>
+              {loginInfo.isLogin ? (
+                <Logout onClick={handleLogout}>logout</Logout>
+              ) : (
+                <Login to="/login">login</Login>
+              )}
+            </Nav>
+            <Logo src="./botLogo.png"></Logo>
+            <MenuWrapper>
+              <Menu to="/invetory">내 보관함</Menu>
+            </MenuWrapper>
+          </SlideContents>
         </SideMenu>
       </Container>
     </>
   );
 }
+const Logout = styled.div`
+  cursor: pointer;
+`;
+const Video = styled.video``;
+const SomeImg = styled.img`
+  width: 100%;
+  border-radius: 15px 15px 15px 15px;
+`;
+const TodayImg = styled.img`
+  width: 100%;
+  border-radius: 15px 15px 15px 15px;
+`;
+const Logo = styled.img`
+  width: 30%;
+  margin: 0 auto;
+`;
+const Icon = styled(BsJustify)`
+  place-self: center;
+`;
 const sideMenu = () => keyframes`
 0%{
-	margin-left: 663px;
+	margin-left: 660px;
 	
 }
 100%{
-	margin-left: 200px;
+	margin-left: 100px;
 }
 `;
 const Login = styled(Link)``;
 const Nav = styled.div`
-  text-align-last: end;
+  text-align-last: start;
   width: 100%;
   font-size: 1.4rem;
+  padding: 2%;
 `;
 const SlideButton = styled.span`
   width: 40px;
   height: 80px;
   margin-top: 80px;
+  display: flex;
   border-radius: 10px 0px 0px 10px;
   background: #9f9f9f8c;
 `;
 const SlideContents = styled.span`
   width: 95%;
+  text-align: center;
   heigth: 100%;
   background: green;
 `;
 const SideMenu = styled.span`
-  width: 600px;
+  width: 660px;
   height: 100%;
-  margin-left: 663px;
+  margin-left: 660px;
   display: flex;
   position: absolute;
   &:hover {
     animation: ${(props) => sideMenu()} 1s ease-in-out;
-    margin-left: 200px;
+    margin-left: 100px;
   }
   animation-fill-mode: forwards;
 `;
@@ -70,8 +139,8 @@ const Title = styled.div`
 const Container = styled.div`
   width: 100%;
   height: 100vh;
-  background: red;
-  overflow: hidden;
+  background: url(./back.jpg);
+  // overflow: hidden;
   position: relative;
   display: flex;
 `;
@@ -80,28 +149,35 @@ const Wrapper = styled.div`
   height: 95%;
   margin: 0 auto;
   align-self: center;
-
-  background: blue;
 `;
 const TodayTarot = styled(Link)`
   height: 100%;
-  background: green;
   ${({ theme }) => theme.common.defaultCardDiv}
 `;
 const SomeTarot = styled(Link)`
   height: 100%;
-  background: orange;
   ${({ theme }) => theme.common.defaultCardDiv}
 `;
 const SubTarotCard = styled.div`
   height: 40%;
   width: auto;
   display: grid;
+  padding-top: 4%;
   grid-template-columns: repeat(2, 1fr);
   grid-gap: 3%;
   grid-template-row: repeat(1, 1fr);
 `;
 const SliderWrapper = styled.div`
   width: 100%;
-  height: 57%;
+  height: 54%;
+  overflow: hidden;
+
+  padding-bottom: 5%;
+`;
+const Menu = styled(Link)`
+  background: white;
+`;
+const MenuWrapper = styled.div`
+  width: 100%;
+  background: green;
 `;
