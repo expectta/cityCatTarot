0a3a6854b50371177d959c4c408630d8a02bb9e0
김지운 <expect.ta@gmail.com>
2021-04-10 23:43:56 +0900

    [CLIENT] ADD : SignUp Component 추가
    SignUp Component 추가

--- src/pages/Index.ts
+++ src/pages/Index.ts
@@ -3,5 +3,6 @@ import Inventory from "./Inventory";
 import Chat from "./Chat";
 import Login from "./Login";
 import UserInfo from "./UserInfo";
+import SignUp from "./SignUp";
 
-export { Main, Inventory, Chat, Login, UserInfo };
+export { Main, Inventory, Chat, Login, UserInfo, SignUp };
