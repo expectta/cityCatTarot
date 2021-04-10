f5dceb7318647866cf025d8889d7f6dfe38e4b4f
김지운 <expect.ta@gmail.com>
2021-04-10 23:51:48 +0900

    [CLIENT] ADD : 회원가입, 로그인 시 유효성 검사 함수 추가
    회원가입, 로그인 시 유효성 검사 함수 추가

---
+++ src/utils/validation.ts
@@ -0,0 +1,25 @@
+// 입력값이 존재하는지 여부 확인
+export function isEmpty(value) {
+  return value.length === 0;
+}
+export function moreThanLength(str, n) {
+  return str.length >= n;
+}
+// 영어 또는 숫자만 가능
+export function onlyNumberAndEnglish(str) {
+  return /^[A-Za-z][A-Za-z0-9]*$/.test(str);
+}
+// 최소 8자 이상하면서, 알파벳과 숫자 및 특수문자(@$!%*#?&) 는 하나 이상 포함
+export function strongPassword(str) {
+  return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(
+    str
+  );
+}
+export function isPhoneNumber(num) {
+  return /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/.test(num);
+}
+export function isEmail(str) {
+  return /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i.test(
+    str
+  );
+}
