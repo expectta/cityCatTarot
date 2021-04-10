4818793e3fcb6fe64ac04301a1462811793ab224
김지운 <expect.ta@gmail.com>
2021-04-10 23:36:10 +0900

    [CLIENT] FIX : Modal 컴포넌트 추가
     Modal 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,82 @@
-import React from "react";
-import { Router, Switch, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
 import theme from "./assets/theme";
 import { createMemoryHistory } from "history";
-import { Main, Login, Inventory, UserInfo, Chat } from "./pages/Index";
-
+import { Main, Login, Inventory, UserInfo, Chat, SignUp } from "./pages/Index";
+import { Modal } from "./components/Index";
 export default function App() {
   const history = createMemoryHistory();
+  const [loginInfo, setLoginInfo] = useState({
+    userId: 0,
+    isLogin: false,
+  });
+  // modal 상태
+  const [modalMessage, setModalMessage] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+  // loading 상태
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  // modal창 제거
+  const closeModal = (login: string) => {
+    if (login) {
+      history.push("/login");
+      setModalVisible(false);
+    }
+    setModalVisible(false);
+  };
+  //modal창 활성화
+  const openModal = (message: string) => {
+    setModalMessage(message);
+    setModalVisible(true);
+  };
+  //로그아웃
+  const handleLogOut = () => {
+    setLoginInfo({
+      userId: 0,
+      isLogin: false,
+    });
+    alert("로그아웃 되었습니다");
+  };
+  //로그인
+  const handleLogin = () => {
+    setLoginInfo({
+      userId: JSON.parse(localStorage.getItem("loginInfo")!).userId,
+      isLogin: true,
+    });
+  };
+  useEffect(() => {
+    if (localStorage.getItem("loginInfo")) {
+      setLoginInfo({
+        userId: JSON.parse(localStorage.getItem("loginInfo")!).userId,
+        isLogin: true,
+      });
+    }
+  }, []);
   return (
-    <Router history={history}>
+    <Router>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Switch>
-          <Route exact path="/" render={() => <Main></Main>}></Route>
-          <Route exact path="/login" render={() => <Login></Login>}></Route>
           <Route
             exact
-            path="/inventory"
+            path="/"
+            render={() => (
+              <Main
+                loginInfo={loginInfo}
+                openModal={openModal}
+                handleLogOut={handleLogOut}
+              />
+            )}
+          ></Route>
+          <Route
+            exact
+            path="/login"
+            render={() => <Login handleLogin={handleLogin}></Login>}
+          />
+          <Route exact path="/signUp" render={() => <SignUp></SignUp>}></Route>
+          <Route
+            exact
+            path="/invetory"
             render={() => <Inventory></Inventory>}
           ></Route>
           <Route
@@ -24,13 +84,43 @@ export default function App() {
             path="/userInfo"
             render={() => <UserInfo></UserInfo>}
           ></Route>
-          <Route exact path="/chat" render={() => <Chat></Chat>}></Route>
+          <Route exact path="/chat" component={Chat}>
+            <Chat
+              openModal={openModal}
+              loginInfo={loginInfo}
+              handleLogOut={handleLogOut}
+            ></Chat>
+          </Route>
         </Switch>
       </ThemeProvider>
+      <Modal visible={modalVisible} closable maskClosable onClose={closeModal}>
+        <h3>{modalMessage}</h3>
+        {loginInfo.isLogin ? (
+          <></>
+        ) : (
+          <GoLogin to="/login" onClick={() => closeModal("login")}>
+            로그인
+          </GoLogin>
+        )}
+      </Modal>
     </Router>
   );
 }
 
+const GoLogin = styled(Link)`
+  background: #6f6eff;
+  width: 50%;
+  display: inline-block;
+  margin-bottom: 1%;
+  font-size: 1rem;
+  color: white;
+  padding: 5px 20px 5px 20px;
+  border: none;
+  border-radius: 20px;
+  outline: 0px;
+  cursor: pointer;
+  text-decoration: none;
+`;
 const GlobalStyle = createGlobalStyle`
 * {
 	box-sizing: border-box;
@@ -41,5 +131,7 @@ body{
 	height:100vh;
 	margin : 0 auto;
 	padding: 0;
+	background:black;
+
 }
 `;
