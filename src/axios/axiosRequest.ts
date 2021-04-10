782fd48d6f100db2dc249963c6bed7282bfc072f
김지운 <expect.ta@gmail.com>
2021-04-10 23:38:49 +0900

    [CLIENT] ADD : 로그인 , 회원가입 API 통신 함수 작성
    로그인 , 회원가입 API 통신 함수 작성

--- src/axios/axiosRequest.ts
+++ src/axios/axiosRequest.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const SERVER_URL = "http://3.36.182.36:8080";
 //모든 카드 리스트
@@ -17,6 +18,65 @@ export function requestCardList() {
       }
     })
     .catch((err) => {
+      console.log(err);
+    });
+  return result;
+}
+
+export function requestLogin(email: string, password: string) {
+  // axios.defaults.headers.common["authorization"] = JSON.parse(
+  //   localStorage.getItem("loginInfo")!
+  // ).accessToken;
+  const result = axios
+    .post(`${SERVER_URL}/session`, { email: email, password: password })
+    .then((res) => {
+      console.log(res, "악시오스 결과");
+      if (res.status === 201) {
+        const accessToken = res.data.accessToken;
+        alert("로그인 되었습니다.");
+        localStorage.setItem(
+          "loginInfo",
+          JSON.stringify({
+            userId: res.data.userId,
+            isLogin: true,
+            accessToken: accessToken,
+          })
+        );
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  return result;
+}
+export function requestSignUp(
+  lastPassword: string,
+  email: string,
+  nickName: string
+) {
+  const result = axios
+    .post(
+      `${SERVER_URL}/register
+		`,
+      {
+        email: email,
+        nickName: nickName,
+        password: lastPassword,
+      }
+    )
+    .then((res) => {
+      console.log(res, "악시오스 결과");
+      if (res.status === 201) {
+        return true;
+      } else {
+        alert("데이터가 없습니다");
+      }
+    })
+    .catch((err) => {
+      return false;
       console.log(err);
     });
   return result;
