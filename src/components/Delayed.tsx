60a6ab9ca3f3745be2bb225992fe75ed09a98def
김지운 <expect.ta@gmail.com>
2021-04-10 23:40:36 +0900

    [CLIENT] FIX : user 버튼 위치 변경 및 css 추가
    FIX : user 버튼 위치 변경 및 css 추가

--- src/components/Delayed.tsx
+++ src/components/Delayed.tsx
@@ -11,15 +11,21 @@ interface Props {
   type: string;
   setChatData: any;
   handleVisiableButton: any;
+  handleNextStep: any;
+  category: any;
+  handlePostCard: any;
 }
 const Delayed = ({
   image,
   timer,
   content,
   avatar,
+  handlePostCard,
   createdAt,
   botName,
   type,
+  category,
+  handleNextStep,
   setChatData,
   handleVisiableButton,
 }: Props) => {
@@ -39,7 +45,7 @@ const Delayed = ({
   }, [show]);
 
   if (!show) return null;
-
+  console.log(category, " = 카테고리");
   return (
     <>
       {type === "user" ? (
@@ -64,12 +70,39 @@ const Delayed = ({
             )}
             <CreatedAt isExist={createdAt}>{createdAt}</CreatedAt>
           </Container>
+          {category === "응!" ? (
+            <ButtonWrapper>
+              <Button onClick={() => handleNextStep()}>{category}</Button>
+            </ButtonWrapper>
+          ) : null}
+          {category === "보관함에 저장" ? (
+            <ButtonWrapper>
+              <Button onClick={() => handlePostCard()}>{category}</Button>
+            </ButtonWrapper>
+          ) : null}
         </>
       )}
     </>
   );
 };
-
+const ButtonWrapper = styled.div`
+  width: 95;
+  display: flex;
+  margin: 0 auto;
+  margin-top: 93px;
+  justify-content: center;
+`;
+const Button = styled.div`
+  width: 200px;
+  text-align: center;
+  cursor: pointer;
+  background: white;
+  margin: 0% 5% 0% 5%;
+  font-size: 1.4rem;
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  padding: 1.6%;
+`;
 export default Delayed;
 const Image = styled.img`
   width: 200px;
@@ -97,21 +130,27 @@ const Avatar = styled.img`
   width: 50px;
 `;
 const ListContent = styled.span`
-  border: 1px solid lightgray;
   max-width: 60%;
-  background: white;
-  margin: 0.5%;
+  background: black;
+  margin: 1.5%;
   font-size: 1.5rem;
   border-radius: 10px;
   padding: 2%;
+  color: white;
+  border: 1px solid #cf5fbf;
+  box-shadow: 0px 0px 11px 2px #6727d7fc;
 `;
 const CreatedAt = styled.span<any>`
   font-size: 1rem;
-  background: white;
+  background: black;
+  color: white;
+
+  box-shadow: 0px 0px 11px 2px #6727d7fc;
+
   ${(props) =>
     props.isExist &&
     css`
-      border: 1px solid lightgray;
+      border: 1px solid #cf5fbf;
       border-radius: 10px;
       place-self: flex-end;
       margin: 0.5%;
