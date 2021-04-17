c7805abf7222c23fed7c7ec771a76d27d800de0c
김지운 <expect.ta@gmail.com>
2021-04-17 20:05:24 +0900

    [CLIENT] ADD : Mycard component 추가
    Mycard component 추가

--- src/pages/Index.ts
+++ src/pages/Index.ts
@@ -4,5 +4,6 @@ import Chat from "./Chat";
 import Login from "./Login";
 import UserInfo from "./UserInfo";
 import SignUp from "./SignUp";
+import MyCard from "./MyCard";
 
-export { Main, Inventory, Chat, Login, UserInfo, SignUp };
+export { Main, Inventory, Chat, Login, UserInfo, SignUp, MyCard };
