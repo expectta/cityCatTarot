dc07f4a05e674dd4ae823d9fb1f7741b78c74e5e
김지운 <expect.ta@gmail.com>
2021-04-17 20:16:48 +0900

    [CLINET] FIX : 0~22, 23~44 까지 난수 생성 로직 추가
    0~22, 23~44 까지 난수 생성 로직 추가

--- src/utils/randomNumber.ts
+++ src/utils/randomNumber.ts
@@ -1,3 +1,11 @@
-export function randomNumber() {
-  return Math.floor(Math.random() * (22 - 0) + 0);
+export function randomNumber(chatType: number) {
+  console.log(chatType, " 카드 타입");
+  let result = 0;
+  if (chatType === 0) {
+    result = Math.floor(Math.random() * (22 - 0) + 0);
+  }
+  if (chatType === 1) {
+    result = Math.floor(Math.random() * (44 - 23) + 23);
+  }
+  return result;
 }
